31633e9864b30ecb551cba0aff0fe1d07410c04a
rkhatri <r.khatri@outlook.com>
2019-12-08 20:13:18 -0600

    Comments edited

--- Week2_Billing/Billing.py
+++ Week2_Billing/Billing.py
@@ -6,17 +6,17 @@
 
 greeting_message = "Welcome to Installation Cost Calculation."  # initializing greeting message
 company_name = input("Enter the name of your company: ")  # Getting company name from user
-fiberoptic_length = float(
+desired_length = float(
     input("Enter desired length of fiber optic cable (in feet): "))  # Getting length from user and converting input to float
 cost_per_feet = 0.87  # initializing cost per feet
 
-total_cost = fiberoptic_length * cost_per_feet  # Total cost calculation
+total_cost = desired_length * cost_per_feet  # Total cost calculation
 
 
 print("\n\n########################################################\n\n"
-      " ********* Invoice for {} *********\n\n"
+      " ********* Installation Receipt for {} *********\n\n"
       " Total Length: {} feet\n"
       " Cost per feet: ${}\n\n"
-      " Total installation cost: ${:,.2f}\n\n"
+      " Total Installation cost: ${:,.2f}\n\n"
       "########################################################"
-      .format(company_name, fiberoptic_length, cost_per_feet, total_cost))
+      .format(company_name, desired_length, cost_per_feet, total_cost))
